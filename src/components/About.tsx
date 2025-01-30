54c640b770e1226653daa691f3f385616d95adb0
tahaalvi212 <m.tahaalvi22@gmail.com>
2025-01-30 16:12:37 +0500

    add files

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,6 +13,21 @@
 
 // export default About
 
+// import React from 'react';
+
+// const About = () => {
+//   return (
+//     <div id="about" className="container pt-32">
+//       <h2 className="text-4xl md:text-5xl">About Me</h2>
+//       <p className="text-gray-500 pt-4">
+//         I am a student at GIAIC and a car enthusiast. I have command on these languages: HTML, CSS, TypeScript, Next.js, and React. As I'm a car enthusiast and I like cars, I made a portfolio including cars. Hope you like my Portfolio.
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default About;
+
 import React from 'react';
 
 const About = () => {
@@ -20,7 +35,7 @@ const About = () => {
     <div id="about" className="container pt-32">
       <h2 className="text-4xl md:text-5xl">About Me</h2>
       <p className="text-gray-500 pt-4">
-        I am a student at GIAIC and a car enthusiast. I have command on these languages: HTML, CSS, TypeScript, Next.js, and React. As I'm a car enthusiast and I like cars, I made a portfolio including cars. Hope you like my Portfolio.
+        I am a student at GIAIC and a car enthusiast. I have command on these languages: HTML, CSS, TypeScript, Next.js, and React. As I&apos;m a car enthusiast and I like cars, I made a portfolio including cars. Hope you like my Portfolio.
       </p>
     </div>
   );
